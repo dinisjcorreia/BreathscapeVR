382f102468d247838f54b798fdcddbd29ba6a84c
Dinis Correia <151139018+dinisjcorreia@users.noreply.github.com>
2024-05-22 19:51:09 +0100

    Update config.inc.php

--- EXTRA/breathscapevr/config.inc.php
+++ EXTRA/breathscapevr/config.inc.php
@@ -2,10 +2,10 @@
 @session_start();
 global $arrConfig;
 
-$arrConfig['servername'] = 'sql.freedb.tech';
-$arrConfig['username'] = 'freedb_kodin';
-$arrConfig['password'] = 'a$S2$U8z*?cux*a';
-$arrConfig['dbname'] = 'freedb_breathscapevr';
+$arrConfig['servername'] = '';
+$arrConfig['username'] = '';
+$arrConfig['password'] = '';
+$arrConfig['dbname'] = '';
 
 
 // chamada de outros include
